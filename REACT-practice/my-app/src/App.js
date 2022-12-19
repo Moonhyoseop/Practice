c4c0b490c064d5388c297e34b79ff1fd6b0aa749
문효섭 <gytjq0723@naver.com>
2022-12-19 23:19:10 +0900

    no message

--- REACT-practice/my-app/src/App.js
+++ REACT-practice/my-app/src/App.js
@@ -1,32 +1,16 @@
 import React from "react";
-import {Routes, Route, Link} from "react-router-dom" ;
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Counter from "./pages/Counter";
-import Input from "./pages/input";
-import Input2 from "./pages/input2";
-import List from "./pages/list";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 
-
-function App() {
-  return (
-    <div className="App">
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/counter">Counter</Link> |
-        <Link to="/input">Input</Link> | <Link to="/input2">Input2</Link> | <Link to="/list">List</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/counter" element={<Counter />}/>
-        <Route path="/input" element={<Input />}/>
-        <Route path="/input2" element={<Input2 />}/>
-        <Route path="/list" element={<List />}/>
-      </Routes>
-    </div>
-  );
+function App(){
+ return(
+  <div className="todo-app">
+    <TodoList />
+  </div>
+ )
 }
+
 
 export default App;
